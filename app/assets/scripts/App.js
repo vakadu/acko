e176a659f1184e7657c8eb8a911e75624d4d9fa3
vakadu <vakadu10@gmail.com>
2019-03-27 16:01:21 +0530

    new chages

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,5 +1,7 @@
 import MobileMenu from './modules/MobileMenu';
 import Tabs from './modules/Tabs';
+import StickyHeader from './modules/StickyHeader';
 
 let mobileMenu   = new MobileMenu();
 let tabs = new Tabs();
+let stickyHeader = new StickyHeader();
--- app/temp/scripts/Plugin.js
+++ app/temp/scripts/Plugin.js
@@ -60,7 +60,7 @@
 /******/ 	__webpack_require__.p = "";
 /******/
 /******/ 	// Load entry module and return exports
-/******/ 	return __webpack_require__(__webpack_require__.s = 4);
+/******/ 	return __webpack_require__(__webpack_require__.s = 6);
 /******/ })
 /************************************************************************/
 /******/ ([
@@ -68,20 +68,22 @@
 /* 1 */,
 /* 2 */,
 /* 3 */,
-/* 4 */
+/* 4 */,
+/* 5 */,
+/* 6 */
 /***/ (function(module, exports, __webpack_require__) {
 
 "use strict";
 
 
-__webpack_require__(5);
-
-__webpack_require__(6);
-
 __webpack_require__(7);
 
+__webpack_require__(8);
+
+__webpack_require__(9);
+
 /***/ }),
-/* 5 */
+/* 7 */
 /***/ (function(module, exports, __webpack_require__) {
 
 "use strict";
@@ -333,7 +335,7 @@ var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol
 })(window, document);
 
 /***/ }),
-/* 6 */
+/* 8 */
 /***/ (function(module, exports) {
 
 (function(window, factory) {
@@ -1053,7 +1055,7 @@ var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol
 
 
 /***/ }),
-/* 7 */
+/* 9 */
 /***/ (function(module, exports, __webpack_require__) {
 
 var __WEBPACK_AMD_DEFINE_RESULT__;/*! picturefill - v3.0.2 - 2016-02-12
